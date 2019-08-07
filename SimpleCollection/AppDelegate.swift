35cd12b0f4ed7ebb191f62e5e313b2941aed1882
Larry Nathaniel Mcdowell <nathaniel29651@gmail.com>
2019-08-07 19:21:05 -0400

    New version isn't simple = small tweaks so that the CollectionView controller is really just a generic UIViewController, whose view has been replaced with a newly created/instantiated collection view - big whoop - No moveItemAt functionality this way either

--- SimpleCollection/AppDelegate.swift
+++ SimpleCollection/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-       let navController = UINavigationController(rootViewController: cv(collectionViewLayout: UICollectionViewLayout()))
+      // let navController = UINavigationController(rootViewController: cv(collectionViewLayout: UICollectionViewLayout()))
+        let navController = UINavigationController(rootViewController: expcv())
         window = UIWindow()
         window?.makeKeyAndVisible()
         window?.rootViewController = navController
---
+++ SimpleCollection/Experimenting.swift
@@ -0,0 +1,121 @@
+//
+//  cv.swift
+//  SimpleCollection
+//
+//  Created by Larry Mcdowell on 8/7/19.
+//  Copyright © 2019 Larry Mcdowell. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+//class cv: UICollectionViewController {
+class expcv: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    //    override init(collectionViewLayout: UICollectionViewLayout){
+    //        super.init(collectionViewLayout: collectionViewLayout)
+    //
+    //    }
+    //
+    //    required init?(coder aDecoder: NSCoder) {
+    //        fatalError("init(coder:) has not been implemented")
+    //    }
+    //
+    
+    var collectionView:UICollectionView!
+    var data:[String] = ["hey","bye","hola","yep","yesterday", "today", "morrow", "simple","hard","easy","tough"]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let layoutOne = UICollectionViewFlowLayout()
+        layoutOne.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+        layoutOne.itemSize = CGSize(width: 150, height: 50)
+        
+        self.collectionView = UICollectionView(frame: CGRect(x: 50, y: 0, width: 300, height: 300), collectionViewLayout: layoutOne)
+        collectionView.backgroundColor = .green
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        // Register cell classes
+        self.collectionView!.register(myCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.collectionView.frame = CGRect(x: 80, y: 60, width: UIScreen.main.bounds.width - 160, height: UIScreen.main.bounds.height - 80)
+        
+        //CGSize(width: 50, height: 50)
+        collectionView.isUserInteractionEnabled = true
+        self.view = collectionView
+        print("here maybe?")
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    // MARK: UICollectionViewDataSource
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        print("here")
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! myCell
+        
+        cell.text = data[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let temp = data[sourceIndexPath.item]
+        data.remove(at: sourceIndexPath.item)
+        data.insert(temp, at: destinationIndexPath.item)
+    }
+    // MARK: UICollectionViewDelegate
+    
+    /*
+     // Uncomment this method to specify if the specified item should be highlighted during tracking
+     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment this method to specify if the specified item should be selected
+     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+     
+     }
+     */
+    
+}
